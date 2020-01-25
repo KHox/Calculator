ff581fe280ce749ecc31fdd942ed7d9cd84620c0
KHox <khoxgmai1@gmail.com>
2020-01-25 15:30:05 +0500

    Small patches
    Small bugfix

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -8,11 +8,13 @@ class Calculator {
       if (window.innerWidth <= 550 && !this.isPhoneMod) {
         this.isPhoneMod = true;
         display.setAttribute('colspan', 3);
-        this.goToPhoneMod();
+        this.hideHidebleButtons();
+        this.hideMenu();
       } else if (window.innerWidth > 550 && this.isPhoneMod) {
         this.isPhoneMod = false;
         display.setAttribute('colspan', 5);
-        this.goToPCMod();
+        this.showHidebleButtons();
+        this.hideMenu();
       }
       if (this.menu && this.menu.classList.contains('open')) {
         this.menu.style.width = this.elem.querySelector('td.changeMenu').clientWidth + 'px';
@@ -67,11 +69,12 @@ class Calculator {
   showMenu(e) {
     if (!this.menu || this.menu.isMobile != this.isPhoneMod) this.createMenu(e);
     this.menu.style.width = e.target.clientWidth + 'px';
+    this.menu.style.top = e.target.offsetTop + 'px';
     this.menu.classList.add('open');
   }
 
   hideMenu() {
-    this.menu.classList.remove('open');
+    if (this.menu) this.menu.classList.remove('open');
   }
 
   createMenu() {
@@ -80,161 +83,48 @@ class Calculator {
     menu.isMobile = this.isPhoneMod;
     if (this.isPhoneMod) {
       menu.innerHTML = `<ul>
-      <li data-buttons="MBase">basic</li>
-      <li data-buttons="othFunc">other-f</li>
+      <li data-buttons="mobile">basic</li>
+      <li data-buttons="othfunc">other-f</li>
     </ul>`;
     } else {
       menu.innerHTML = `<ul>
       <li data-buttons="base">basic</li>
-      <li data-buttons="arcFunc">arc-f</li>
+      <li data-buttons="arcfunc">arc-f</li>
     </ul>`;
     }
     this.menu = menu;
     menu.onclick = e => {
       if (e.target.tagName == 'LI') {
         this.hideMenu();
-        this.replaceButtons(Calculator[e.target.dataset.buttons]);
+        this.replaceButtons(e.target.dataset.buttons);
       }
     }
-    document.querySelector('.main-element').append(menu);
+    this.elem.append(menu);
   }
 
-  replaceButtons(buttons) {
-    let r = this.elem.querySelectorAll('td[data-replaceble]');
-    console.log(r);
-    for (let i = 0; i < buttons.length; i++) {
-      if (r[i]) r[i].outerHTML = buttons[i];
+  replaceButtons(type) {
+    let buttons = this.elem.querySelectorAll(`td[data-${type}]`);
+    for (let button of buttons) {
+      button.innerHTML = button.dataset[type];
+      let tc = button.dataset.toggleclass;
+      if (tc) {
+        button.dataset.toggleclass = button.className;
+        button.className = tc;
+      }
     }
   }
 
-  goToPhoneMod() {
-    this.clearLast();
-    this.elem.insertAdjacentHTML('beforeEnd', Calculator.mobileBase);
+  hideHidebleButtons() {
+    let buttons = this.elem.querySelectorAll('[data-hideble]');
+    for (let button of buttons) {
+      button.hidden = true;
+    }
   }
 
-  goToPCMod() {
-    this.clearLast();
-    this.elem.insertAdjacentHTML('beforeEnd', Calculator.PCBase);
-  }
-
-  clearLast() {
-    if (this.menu) this.hideMenu();
-    let rows = this.elem.rows;
-    let length = rows.length;
-    for (let i = length - 1; i > length - 6; i--) {
-      rows[i].remove();
+  showHidebleButtons() {
+    let buttons = this.elem.querySelectorAll('[data-hideble]');
+    for (let button of buttons) {
+      button.hidden = false;
     }
   }
 }
-
-Calculator.base = [
-  '<td class="func" data-replaceble>sin</td>',
-  '<td class="func" data-replaceble>cos</td>',
-  '<td class="func" data-replaceble>tan</td>',
-];
-
-Calculator.arcFunc = [
-  '<td class="func" data-replaceble>asin</td>',
-  '<td class="func" data-replaceble>acos</td>',
-  '<td class="func" data-replaceble>atan</td>',
-];
-
-Calculator.MBase = [
-  '<td class="num" data-replaceble>7</td>',
-  '<td class="num" data-replaceble>8</td>',
-  '<td class="num" data-replaceble>9</td>',
-  '<td class="num" data-replaceble>4</td>',
-  '<td class="num" data-replaceble>5</td>',
-  '<td class="num" data-replaceble>6</td>',
-  '<td class="num" data-replaceble>1</td>',
-  '<td class="num" data-replaceble>2</td>',
-  '<td class="num" data-replaceble>3</td>',
-  '<td class="oper" data-replaceble>^</td>',
-  '<td class="num" data-replaceble>0</td>'
-];
-
-Calculator.othFunc = [
-  '<td class="func" data-replaceble>sin</td>',
-  '<td class="func" data-replaceble>cos</td>',
-  '<td class="func" data-replaceble>tan</td>',
-  '<td class="func" data-replaceble>asin</td>',
-  '<td class="func" data-replaceble>acos</td>',
-  '<td class="func" data-replaceble>atan</td>',
-  '<td class="func" data-replaceble>sqrt</td>',
-  '<td class="func" data-replaceble>ctg</td>',
-  '<td class="spec-func" data-replaceble>|x|</td>',
-  '<td class="spec" data-replaceble>(</td>',
-  '<td class="spec" data-replaceble>)</td>',
-];
-
-Calculator.mobileBase = `<tr>
-  <td class="changeMenu">...</td>
-  <td class="delAll">C</td>
-  <td class="const">π</td>
-  <td class="const">e</td>
-</tr>
-<tr>
-  <td class="num" data-replaceble>7</td>
-  <td class="num" data-replaceble>8</td>
-  <td class="num" data-replaceble>9</td>
-  <td class="oper">+</td>
-</tr>
-<tr>
-  <td class="num" data-replaceble>4</td>
-  <td class="num" data-replaceble>5</td>
-  <td class="num" data-replaceble>6</td>
-  <td class="oper">−</td>
-</tr>
-<tr>
-  <td class="num" data-replaceble>1</td>
-  <td class="num" data-replaceble>2</td>
-  <td class="num" data-replaceble>3</td>
-  <td class="oper">×</td>
-</tr>
-<tr>
-  <td class="oper" data-replaceble>^</td>
-  <td class="num" data-replaceble>0</td>
-  <td class="oper">,</td>
-  <td class="oper">÷</td>
-</tr>`;
-
-Calculator.PCBase = `<tr>
-  <td class="changeMenu">...</td>
-  <td class="func">sqrt</td>
-  <td class="delAll">C</td>
-  <td class="const">π</td>
-  <td class="const">e</td>
-  <td class="oper">+</td>
-</tr>
-<tr>
-  <td class="func"></td>
-  <td class="spec-func">|x|</td>
-  <td class="num">7</td>
-  <td class="num">8</td>
-  <td class="num">9</td>
-  <td class="oper">+</td>
-</tr>
-<tr>
-  <td class="func" data-replaceble>sin</td>
-  <td class="func" data-replaceble>cos</td>
-  <td class="num">4</td>
-  <td class="num">5</td>
-  <td class="num">6</td>
-  <td class="oper">−</td>
-</tr>
-<tr>
-  <td class="func" data-replaceble>tan</td>
-  <td class="func">ctg</td>
-  <td class="num">1</td>
-  <td class="num">2</td>
-  <td class="num">3</td>
-  <td class="oper">×</td>
-</tr>
-<tr>
-  <td class="spec">(</td>
-  <td class="spec">)</td>
-  <td class="oper">^</td>
-  <td class="num">0</td>
-  <td class="oper">,</td>
-  <td class="oper">÷</td>
-</tr>`;
